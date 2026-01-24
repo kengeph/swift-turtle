548264e242729fe7b91dccf753fe6e33b3cf5b2f
Kenny Anderson <kengeph@gmail.com>
2026-01-24 07:32:11 -0500

    Revert to absolute paths for InfinityFree hosting

--- couplegames/vite.config.js
+++ couplegames/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths for InfinityFree hosting
+  base: '/couplegames/',
   build: {
     outDir: '../docs/couplegames',
   },
