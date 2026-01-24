13797963a9ffafddbd16579e37f1d75806ff6633
Kenny Anderson <kengeph@gmail.com>
2026-01-24 07:17:08 -0500

    Change base path to relative for InfinityFree hosting compatibility

--- couplegames/vite.config.js
+++ couplegames/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/couplegames/',
+  base: './', // Use relative paths for InfinityFree hosting
   build: {
     outDir: '../docs/couplegames',
   },
